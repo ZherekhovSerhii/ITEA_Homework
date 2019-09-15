288ded65c930663fba4f6eb7c181b8b9e84eba40
ZherekhovSerhii <55210753+ZherekhovSerhii@users.noreply.github.com>
2019-09-15 20:24:30 +0300

    Update main.cpp 1 time
    Заменены названия заголовочных файлов, константы внесены в область функции main, ворнинги убраны, отредактирован внешний вид кода в соответствии с кодстайлом, использован static_cast<> вместо приведения в С-стиле, строка 41 исправлена, условные конструкции заменены на более понятные, отредактирован текст, который выводится на консоль

--- main.cpp
+++ main.cpp
@@ -1,52 +1,56 @@
 #include<iostream>
 #include<limits>
-#include<math.h>
+#include<cmath>
 #include<cstdint>
 
-const short TWO = 2;
-const short FOUR = 4;
+int main(int argc, char const *argv[]) {
+  const short TWO = 2;
+  const short FOUR = 4;
+  constexpr int32_t LOW_LIM = std::numeric_limits<int32_t>::min();
+  constexpr int32_t UP_LIM = std::numeric_limits<int32_t>::max();
 
-int main(int argc, char const argv[]) {
+  std::cout << "This programm is for solving quadratic equation y=a*x^2+b*x+c" << std::endl;
+  std::cout << "Please enter 3 integer coefficients in order from 'a' to 'c' in range ["
+	    << LOW_LIM << ".." << UP_LIM
+	    << "] \n(use space as separator)" << std::endl;
 
-	constexpr int32_t LOW_LIM = std::numeric_limits<int32_t>::min();
-	constexpr int32_t UP_LIM = std::numeric_limits<int32_t>::max();
+  int64_t a, b, c;
 
-	std::cout << "Please enter 3 integer numbers in range [" << LOW_LIM << ".." << UP_LIM << "] (use space as separator)" << std::endl;
+  std::cin >> a >> b >> c;
 
-	int64_t a, b, c;
+  if ((a>UP_LIM)||(a<LOW_LIM)) {
+    std::cout << "You entered an incorrect number for 'a' coefficient, try again and follow the instructions" << std::endl;
+    return 0;
+  }
 
-	std::cin >> a >> b >> c;
+  if ((b>UP_LIM) || (b<LOW_LIM)) {
+    std::cout << "You entered an incorrect number for 'b' coefficient, try again and follow the instructions" << std::endl;
+    return 0;
+  }
 
-	if (((a > UP_LIM) || (b > UP_LIM) || (c > UP_LIM)) || ((a < LOW_LIM) || (b < LOW_LIM) || (c < LOW_LIM))) {
-		std::cout << "You entered an incorrect number, try again and follow the instructions" << std::endl;
-		return 0;
-	}
+  if ((c>UP_LIM) || (c<LOW_LIM)) {
+    std::cout << "You entered an incorrect number for 'c' coefficient, try again and follow the instructions" << std::endl;
+    return 0;
+  }
 
-	if (a == 0) {
-		std::cout << "Dividing by zero error" << std::endl;
-		return 0;
-	}
+  if (a == 0) {
+    std::cout << "Dividing by zero error" << std::endl;
+    return 0;
+  }
 	
-	int64_t D;
-	D = (int64_t)pow(b, 2) - FOUR * a * c;
+  int64_t D;
+  D = (int64_t)pow(b, 2) - FOUR * a * c;
     
-	if (D > 0) {
-		auto x1 = ((-b) + sqrt(D)) / (TWO * a);
-		auto x2 = ((-b) - sqrt(D)) / (TWO * a);
-		std::cout << "x1= " << x1 <<" x2= " << x2 << std::endl; 
-		return 0;
-	}
+  if (D > 0) {
+    auto x1 = ((-b) + sqrt(D)) / (TWO * a);
+    auto x2 = ((-b) - sqrt(D)) / (TWO * a);
+    std::cout << "x1= " << x1 <<" x2= " << x2 << std::endl;
+  } else if (D == 0) {
+    auto x = (-b + 0.f) / (TWO * a);
+    std::cout << "x= " << x << std::endl; 
+  } else if (D < 0) {
+    std::cout << "Answer is irrational" << std::endl; 
+  }
 
-	if (D == 0) {
-		auto x = (-b) / (TWO * a);
-		std::cout << "x= " << x << std::endl; 
-		return 0;
-	}
-
-	if (D < 0) {
-		std::cout << "Answer is irrational" << std::endl; 
-		return 0;
-	}
-
-	return 0;
+  return 0;
 }
