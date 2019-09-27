771ec81f64e2f2a868fa4d88f868a1e7dd45c1e5
ZherekhovSerhii <55210753+ZherekhovSerhii@users.noreply.github.com>
2019-09-27 16:51:47 +0300

    No. 4 homework for editing text (is_digit, to_upper, to_lower and reverse function realisations)
    Реализованы функции: 
    -поиска числа в строке (is_digit);
    -перевода символов из нижнего регистра в верхний (to_upper);
    -перевода символов из верхнего регистра в нижний (to_lower);
    -реверса строки (reverse)

---
+++ to_lower.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+  constexpr uint16_t dist_between_regs =
+      static_cast<int>('a') - static_cast<int>('A');
+  constexpr unsigned SIZE = 256;
+  char mass[SIZE] = "";
+
+  std::cout << "Enter the string: ";
+  std::cin >> mass;
+
+  for (size_t i = 0; mass[i] != '\0'; ++i) {
+    if ((mass[i] >= 'A') && (mass[i] <= 'Z'))
+      mass[i] += dist_between_regs;
+  }
+  std::cout << "Edited string: " << mass;
+  return 0;
+}
