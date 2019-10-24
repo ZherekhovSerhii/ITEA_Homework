a02e69a74a32d0a8dd7f0d6a0fc00888f18dd808
ZherekhovSerhii <55210753+ZherekhovSerhii@users.noreply.github.com>
2019-10-24 12:56:09 +0300

    No.7 homework переписал односвязный список на двусвязный, на примере STL list`а

--- list.cpp
+++ list.cpp
@@ -1,21 +1,39 @@
 #include "List.hpp"
 #include <iostream>
+#include <list>
 
 int main(int argc, char *argv[]) {
-  List<int> l1;
-  l1.add(40);
-  l1.add(30);
-  l1.insert(35, 1);
 
-  List<int> l2;
-  l2.add(789);
-  l2.add(79);
-  l2 = l1;
+  List<int> l1;
+  l1.push_front(40);
+  l1.push_back(37);
+  l1.push_front(35);
+  l1.push_back(20);
+  l1.push_back(30);
 
   std::cout << "L1:\n";
   l1.print();
+
+  List<int> l2(l1);
+  l2.emplace(l2.find(40), 41);
+  l2.delete_back();
   std::cout << "L2:\n";
   l2.print();
 
+  List<int> l3;
+  l3.push_back(1);
+  l3.insert(l3.find(1), 2);
+  l3.insert(l3.find(2), 3);
+  l3.delete_element(l3.find(2));
+  l3.delete_back();
+  l3.delete_front();
+
+  std::cout << "L3:\n";
+  l3.print();
+
+  // std::cout << "l_begin: " << l3.l_begin->field << " l_end: " <<
+  // l3.l_end->field << "\n";
+
+  //std::cout << "size: " << l3.size();
   return 0;
 }
